9a42872c60f60a51f59242b06e1c3fc980299e1c
Harveyda94 <141647124+Harveyda94@users.noreply.github.com>
2026-02-24 18:48:24 +0000

    Update resistance_network_pipeline.py

--- examples/resistance_network_pipeline.py
+++ examples/resistance_network_pipeline.py
@@ -1,3 +1,9 @@
+#Ability to compare datasets - Dave's suggestion
+#Summarise by BO in statistics
+#Resistance should be from start of arteriole to end of venule
+#Mean distance of object (classifier) to each capillary type and BO
+#Overall list of every vessel and its properties
+
 #!/usr/bin/env python3
 """Refactored full pipeline example using ImageLynx package."""
 import logging
