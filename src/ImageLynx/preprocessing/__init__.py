d1570ca5672f4996e25f86ce508c6ebb4fda26c2
Harveyda94 <141647124+Harveyda94@users.noreply.github.com>
2026-03-03 14:15:38 +0000

    Update __init__.py

--- src/ImageLynx/preprocessing/__init__.py
+++ src/ImageLynx/preprocessing/__init__.py
@@ -1,8 +1,9 @@
-"""Preprocessing: skeleton cleaning, bridging, skeletonization."""
-from .skeleton import bridge_gaps, load_and_skeletonize_3d_tif, load_and_skeletonize_3d_h5
+"""Preprocessing: loading, skeletonisation, skeleton cleaning, bridging."""
+from .skeleton import bridge_gaps, load_and_skeletonize_3d_tif, load_and_skeletonize_3d_h5, simplify_to_3d
 
 __all__ = [
     "load_and_skeletonize_3d_tif",
     "load_and_skeletonize_3d_h5",
     "bridge_gaps",
+    "simplify_to_3d"
 ]
