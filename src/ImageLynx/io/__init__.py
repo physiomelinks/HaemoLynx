47c238db87cd1710c8fdfcf5c740e942d068a45f
Finbar Argus <farg967@aucklanduni.ac.nz>
2026-03-06 18:10:23 +1300

    feat(#21) adding zip file for example nerve tiff and allowing

--- src/ImageLynx/io/__init__.py
+++ src/ImageLynx/io/__init__.py
@@ -3,6 +3,7 @@ from .load import (
     crop_tiff_volume_from_corners,
     load_and_skeletonize_3d_tif,
     load_and_skeletonize_3d_h5,
+    resolve_image_path_with_optional_zip,
     simplify_to_3d,
 )
 from ..preprocessing.skeleton import bridge_gaps
@@ -11,6 +12,7 @@ __all__ = [
     "load_and_skeletonize_3d_tif",
     "load_and_skeletonize_3d_h5",
     "crop_tiff_volume_from_corners",
+    "resolve_image_path_with_optional_zip",
     "bridge_gaps",
     "simplify_to_3d",
 ]
